88b7a62ddeadeed154f5c6db1ee4a721b821c2ce
ShahRay <brokenbrain777@gmail.com>
2020-09-01 11:23:38 +0300

    Add files via upload

--- Test_example/WorkerBack/Test_Example.Back.Messaging/Options/RabbitMqConfiguration.cs
+++ Test_example/WorkerBack/Test_Example.Back.Messaging/Options/RabbitMqConfiguration.cs
@@ -5,8 +5,5 @@ namespace Test_Example.Back.Messaging.Options
     public class RabbitMqConfiguration
     {
         public string Hostname { get; set; }
-        public string UserName { get; set; }
-        public string Password { get; set; }
-        public int Port { get; set; }
     }
 }
